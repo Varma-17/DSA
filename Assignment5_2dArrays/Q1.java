63a2bdc0e7abaa0910030514469d691f4ec32820
Varma-17 <86680384+Varma-17@users.noreply.github.com>
2023-07-10 17:38:59 +0530

    Create Q1.java

---
+++ Assignment5_2dArrays/Q1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        int[][] res=new int[m][n];
+        if(m*n!=original.length)
+        {
+            return new int[0][0];
+        }
+        for(int i=0;i<original.length;i++)
+        {
+            res[i/n][i%n]=original[i];
+        }
+        return res;
+    }
+}
