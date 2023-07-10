5596208f908fb95a3692b9b85985a84d8a541a6b
Varma-17 <86680384+Varma-17@users.noreply.github.com>
2023-07-10 17:40:34 +0530

    Create Q4.java

---
+++ Assignment5_2dArrays/Q4.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Integer>> findDifference(int[] nums1, int[] nums2) {
+        HashSet<Integer> set1=new HashSet<>();
+        HashSet<Integer> set2=new HashSet<>();
+
+        for(int i:nums1)
+        {
+            set1.add(i);
+        }
+        for(int i:nums2)
+        {
+            set2.add(i);
+        }
+
+        List<List<Integer>> res=new ArrayList<>();
+        res.add(new ArrayList<>());
+        res.add(new ArrayList<>());
+        for(int i:set1)
+        {
+            if(!set2.contains(i))
+            {
+                res.get(0).add(i);
+            }
+        }
+        for(int i:set2)
+        {
+            if(!set1.contains(i))
+            {
+                res.get(1).add(i);
+            }
+        }
+        return res;
+    }
+}
