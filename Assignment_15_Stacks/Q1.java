8add17b9ee6836a71ee20b77c15cce02534f0479
Varma-17 <86680384+Varma-17@users.noreply.github.com>
2023-07-13 21:34:41 +0530

    Create Q1.java

---
+++ Assignment_15_Stacks/Q1.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class Main
+{
+    public static int[] next(int[] arr)
+    {
+        Stack<Integer> st=new Stack<>();
+        
+        int[] res=new int[arr.length];
+        Arrays.fill(res,-1);
+        
+        for(int i=arr.length-1;i>=0;i--)
+        {
+            while(!st.isEmpty())
+            {
+                if(st.peek()<=arr[i])
+                {
+                    st.pop();
+                }
+                else
+                {
+                    res[i]=st.peek();
+                    break;
+                }
+            }
+            st.push(arr[i]);
+        }
+        
+        return res;
+    }
+	public static void main(String[] args) {
+		int[] res = next(new int[]{1,2,3,4,5});
+		for(int i=0;i<res.length;i++)
+		{
+		    System.out.println(res[i]);
+		}
+	}
+}
